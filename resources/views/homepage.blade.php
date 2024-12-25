27ee46d601bbc9843a6329c9365e8b34b0c91fe0
enjaayyy <enujeemontesaarubeosu@gmail.com>
2024-12-25 18:04:07 +0800

    minor UI fixed

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,33 +9,31 @@
              <img src="{{asset('assets/logo.png') }}" class="home-logo" height="50px">
             </div>
             <div class="function-btns">
-        <button type="button" class="home-btn">Home</button>
-        <button type="button" class="contact-btn">Contact</button>
-        <button type="button" class="about-btn">About us</button>
-        <button type="button" class="services-btn">Services</button>
-        <img src= "{{asset('assets/loginlogo.png') }}" class="login-logo">
-        <a href = "{{ route('login') }}" style = "text-decoration:none;">
-        <button type="button" class="login-btn">Log in</button></a><br>
+                <button type="button" class="home-btn">Home</button>
+                <button type="button" class="contact-btn">Contact</button>
+                <button type="button" class="about-btn">About us</button>
+                <button type="button" class="services-btn">Services</button>
+                <img src= "{{asset('assets/loginlogo.png') }}" class="login-logo">
+                <a href = "{{ route('login') }}" style = "text-decoration:none;">
+                <button type="button" class="login-btn">Log in</button></a><br>
             </div>
         </div>
         <div class="homepage-container">
-               <img src="{{asset('assets/cover-page.jpg') }}" class= "homepage-img">
+            <img src="{{asset('assets/cover-page.jpg') }}" class= "homepage-img">
                 <div class="tagline-container">
                     <p class ="tagline">Stay Serene with Serenity</p>
-                    <p class ="tag-descrip">Your pocket companion for your mental well being</p>
+                        <div class="tag-descrip-container">
+                            <p class ="tag-descrip">Serenity is your personal mental health companion, offering tools for immediate support, personal mornitoring, patient analysis and appointment scheduling. Helping you to stay serene everday.</p>
+                        </div>
                     <div class="sign-container">
                         <a href="{{ route('register') }}" style="text-decoration: none;">
-                    <button type="button" class="sign-up">
-                        <img src="{{asset('assets/signuplogo.png') }}" class="signup-logo">    
-                        <p class="sign-text">Sign up</p>
-                        </button>
+                            <button type="button" class="sign-up">
+                                <img src="{{asset('assets/signuplogo.png') }}" class="signup-logo">    
+                                <p class="sign-text">Sign up</p>
+                            </button>
                         </a>
                     </div>
-                
                 </div>
-               
-                    
-             
         </div>
         
       
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,33 +4,32 @@
         <link rel="stylesheet" href="{{ asset('css/register.css') }}">
 </head>
 <body>
-    <div>
-        <a href ="{{ route('home') }}" style = "text-decoration: none;">
-        <img src="{{asset('assets/logo.png') }}" class="register-logo">
-</a>
-</div>
-<div class = "register-container">
-    <div class = "register-body">
-        <p class="header"> SIGN-UP </p>
-        <form action="{{ url('register') }}" method="POST">
-            @csrf
-            <p class="name-header">Full Name</p>
-            <input type = "text" class = "fname-input" placeholder="First Name, Middle Name, Last Name" name="fullname">
-            <p class="email-header">Email</p>
-            <input type = "text" class = "email-input" placeholder="Email" name="email">
-            <p class="password-header">Password</p>
-            <input type = "password" class = "pass-input" placeholder="Password"  name="password">
-            <a href="{{ url('register-details') }}" style="text-decoration: none;">
-             <button class="next-btn" type="submit">Next -></button>
+    <div class = "register-container">
+        <div class = "register-body">
+            <a href ="{{ route('home') }}" style = "text-decoration: none;">
+            <img src="{{asset('assets/logo-w-text.svg') }}" class="register-logo">
             </a>
-</form>
-
-    <p class ="prompt">Already have an Account? <a href="{{ route('login') }}" style = "color: blue;">Click here! </a></p>
-</div>
-    <div class = "register-art">
-        <img src="{{asset('assets/register-art.png') }}" class="register">
-</div>
-</div>
-
+                <div class="register-form">
+                    <p class="header"> SIGN-UP </p>
+                    <form action="{{ url('register') }}" method="POST">
+                        @csrf
+                            <p class="name-header">Full Name</p>
+                            <input type = "text" class = "fname-input" placeholder="First Name, Middle Name, Last Name" autocomplete="off" name="fullname" required>
+                            <p class="email-header">Email Address</p>
+                            <input type = "text" class = "email-input" placeholder="Email"  autocomplete="off" name="email" required>
+                            <p class="password-header">Password</p>
+                            <input type = "password" class = "pass-input" placeholder="Password"  name="password" required>
+                            <a href="{{ url('register-details') }}" style="text-decoration: none;"><br>
+                                <button class="next-btn" type="submit">Next -></button>
+                            </a>
+                    </form>
+                    <p class ="prompt">Already have an Account? <a href="{{ route('login') }}" style = "color: blue;">Click here! </a></p>
+                </div>
+        </div>
+            <div class = "register-art">
+                <p class="art-header">Welcome</p>
+                <p class="art-text">Join Serenity and be a part of the solution that the world needs!</p>
+            </div>
+    </div>
 </body>
-    </html>
+</html>
