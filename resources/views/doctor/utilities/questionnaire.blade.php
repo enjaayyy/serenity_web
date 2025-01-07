08a151d79380a81afaaea97789e8e1503ca6daa9
enjaayyy <enujeemontesaarubeosu@gmail.com>
2025-01-07 17:52:37 +0800

    view questions in progress

--- resources/views/doctor/utilities/questionnaire.blade.php
+++ resources/views/doctor/utilities/questionnaire.blade.php
@@ -1,6 +1,6 @@
  <div id="questionnaire-container" style="display: block;">
        <div id="qst-cat-btns">
-            @foreach($doctorData['questions'] as $category => $subCategory)
+            {{-- @foreach($doctorData['questions'] as $category => $subCategory)
                 <h2>{{$category}}</h2>
                 <ul>
                     @foreach($subCategory as $subCategories => $questions)
@@ -16,13 +16,59 @@
                     </li>
                     @endforeach
                 </ul>
-            @endforeach
+            @endforeach --}}
        </div>
+       <div id="qst-content"></div>
+       <div id="qst-categories">
+       </div>
+       <div id="qqqq"></div>
         <script>
             var doctorQuestions = @json($doctorData);
-            const Questions =  doctorQuestions.questions;
+            const questionsKeys =  Object.keys(doctorQuestions.questions);
 
-            console.log(Questions);
+            const questionButtons = document.getElementById('qst-cat-btns');
+            const questionContents = document.getElementById('qst-content');
+            const questionSubCategories = document.getElementById('qst-categories');
+            const qqqq = document.getElementById('qqqq');
+
+            console.log(doctorQuestions.questions);
             
+            questionsKeys.forEach(key => {
+                const button = document.createElement("button");
+                button.textContent = key;
+                button.addEventListener("click", () => displayQuestionTitle(key));
+
+                questionButtons.appendChild(button);
+            })
+
+            function displayQuestionTitle(titleKey) {
+
+                const title = Object.keys(doctorQuestions.questions[titleKey]);
+
+                questionContents.innerHTML = " ";
+                questionSubCategories.innerHTML = " ";
+                title.forEach(questionTitle => {
+                    const titleText = document.createElement("p");
+                    titleText.textContent = questionTitle;
+                    questionContents.appendChild(titleText);
+                })
+
+                const subCategories = Object.keys(doctorQuestions.questions[titleKey][title]);
+
+                subCategories.forEach(subCat => {
+                    const categories = document.createElement("p");
+                    categories.textContent = subCat;
+                    questionSubCategories.appendChild(categories);
+                        
+                        const questionHeaders = Object.keys(doctorQuestions.questions[titleKey][title][subCat]);
+                        questionHeaders.forEach(quest => {
+                            const questheads = document.createElement("p");
+                            questheads.textContent = quest;
+                            questionSubCategories.appendChild(questheads);
+                        })
+                });
+            }
+
+           
         </script>
     </div>
