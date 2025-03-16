97520883d95b22ee624ac17312b2eb327652b5c7
Nj Albeos <enujeemontesaarubeosu@gmail.com>
2025-03-17 00:54:03 +0800

    appointments feature in progress

--- resources/views/doctor/appointments.blade.php
+++ resources/views/doctor/appointments.blade.php
@@ -3,11 +3,28 @@
 @section('title', 'doctor-patient-appointments')
 
 @section('content')
-    <div id='calendar'></div>
-    <button id="add-event">add event</button>
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
-    <script>
+    <link rel="stylesheet" href="{{ asset('css/doctor/appointments.css') }}">
+    <div class="container">
+      <div class="admin-header">
+          <p class="dash-text">Appointments</p>
+          <img src="{{asset('assets/logo-w-text.svg') }}" class="page-logo">
+      </div>
+      <div class="main-content">
+        <div id='calendar' class="calendar"></div>
+        <div id='appointments' class="appointments-container">
+          <div class="schedules-container" id="schedules-container">
 
+          </div>
+          <button id="add-event" class="add-event">
+            Add Event
+          </button>
+        </div>
+      </div>
+    </div>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+    <script src="{{ asset('js/doctor/addschedule.js') }}"></script>
+    <script>
+      let calendar;
       document.addEventListener('DOMContentLoaded', function() {
         let calendarEl = document.getElementById('calendar');
         let calendar = new FullCalendar.Calendar(calendarEl, {
@@ -15,12 +32,12 @@
         });
         calendar.render();
 
+        // displayCalendarSchedules(calendar);
+
         let addEvent = document.getElementById('add-event');
         addEvent.addEventListener("click", function(){
-            calendar.addEvent({
-                title: 'Meeting with John',
-                start: '2025-03-20',
-            });
+          addSchedule();
+            
         })
       });
 
